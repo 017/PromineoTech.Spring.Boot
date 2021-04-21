c7fb87e6aaca2129b2e865d4dbf30e4f25792dab
George Heeres <gheeres@gmail.com>
2021-04-20 22:24:33 -0500

    test: Fix failing unit test due to missing path/mapping

--- src/main/java/com/promineotech/jeep/controllers/BasicJeepSalesController.java
+++ src/main/java/com/promineotech/jeep/controllers/BasicJeepSalesController.java
@@ -1,11 +1,12 @@
 package com.promineotech.jeep.controllers;
 
 import java.util.List;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.promineotech.jeep.entity.Jeep;
 
-@RestController
+@RestController()
+@RequestMapping("/jeeps")
 public class BasicJeepSalesController implements JeepSalesController {
   public List<Jeep> fetchJeeps(String model, String trim) {
     return null;
