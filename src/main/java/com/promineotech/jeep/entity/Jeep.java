93417815a5183e602663046f12e04c18eea15e42
George Heeres <gheeres@gmail.com>
2021-04-20 22:20:53 -0500

    fix: Fix jackson/lombok error with no default constructor

--- src/main/java/com/promineotech/jeep/entity/Jeep.java
+++ src/main/java/com/promineotech/jeep/entity/Jeep.java
@@ -1,11 +1,15 @@
 package com.promineotech.jeep.entity;
 
 import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Jeep {
   private long modelPK;
   private JeepModel modelId;
